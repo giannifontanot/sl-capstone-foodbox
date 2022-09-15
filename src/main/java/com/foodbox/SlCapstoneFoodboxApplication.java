f5787ac4d1b3a848023951986d608f675d2ebc53
Gianni Fontanot <giannifontanot@gmail.com>
2022-09-15 05:29:59 -0500

    Initial commit

---
+++ src/main/java/com/foodbox/SlCapstoneFoodboxApplication.java
@@ -0,0 +1,13 @@
+package com.foodbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlCapstoneFoodboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlCapstoneFoodboxApplication.class, args);
+    }
+
+}
