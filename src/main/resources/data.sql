f2a0cc16f509ad555b642f6c6542566c6ef2d786
Gianni Fontanot <giannifontanot@gmail.com>
2022-10-16 05:42:02 -0500

    Database update that contains 50 dishes.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,123 @@
 
 
-INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)values(1, 'ARAB', 'rico Kebab',
-                                                                                           'http://tinyurl
-.com/kebab2Image','Y', 'Kebab', 10);
-INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)values(5, 'SPAIN', 'Delicious
-rice with fried shimp to perfection',
-                                                                                           'http://tinyurl
-.com/shrimpFriedRiceImage','Y', 'Shrimp Fried Rice', 50);
-INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)values(2, 'MEXICAN', 'Borrego
-asado en horno de hoyo ',
-                                                                                           'http://tinyurl
-.com/barbacoaImage','Y', 'Tacos de Barbacoa', 20);
-INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)values(3, 'MEXICAN', 'Receta de la abuelita de mole verde', 'http://tinyurl.com/moleVerdeImage','Y', 'Mole Verde', 30);
 
-insert into user(id,contact,email,password, profile, username, credit, name) values (1,"21480945441","elmerhomero@123
-.com", "sitiene","C", "yomerito", "", "Yo soy Yomerito");
-insert into user(id,contact,email,password, profile, username, credit, name) values (2,"21480945441","admin@123.com", "notiene","A", "admin", "", "Yo soy el Admin");
-insert into user(id,contact,email,password, profile, username, credit, name) values (3,"21480945444","tu@123.com", "notiene","C", "user2", "",
-                                                                                     "Yo soy el user2");
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(5, 'ITALIAN', 'Delicious rice with fried shimp to perfection','http://tinyurl.com/shrimpFriedRiceImage','Y', 'Shrimp Fried Rice', 50);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(2, 'MEXICAN', 'Borrego asado en horno de hoyo ','http://tinyurl.com/barbacoaImage','Y', 'Tacos de Barbacoa', 20);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(3, 'MEXICAN', 'Receta de la abuelita de mole verde', 'http://tinyurl.com/moleVerdeImage','Y', 'Exquisite Mexican Mole Verde', 30);
 
 
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(13, 'ASIAN', 'Muu haeng is thinly sliced pork, typically shoulder, while siin haeng is thinly sliced beef, typically a tough cut with fat, like top round.','https://tinyurl.com/haengImage','Y', 'Sun-Dried Beef', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(24, 'ASIAN', 'The meat is mixed with lemongrass, galangal, ginger, and garlic, then stuffed into natural casings.','https://tinyurl.com/herbalPorkImage','Y', 'Sai Oua (Herbal Pork Sausage)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(34, 'ASIAN', 'The mango flesh, bright yellow even before ripening, stays crunchy and provides a fresh sour note absent in green papaya.',        'https://tinyurl.com/mangoSaladImage','Y', 'Tam Muk Muang (Green-Mango Salad)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(28, 'ASIAN', 'Naem khao is a mixture of salt-cured ground pork, pig skin, steamed and dried white rice, and dried shredded coconut',        'https://tinyurl.com/coconutRiceImage','Y', 'Naem Khao (Crispy Coconut Rice)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(43, 'ASIAN', 'Salad made from ground meat and herbs, laced with fish sauce and lime juice and topped with a powder made from dry ground rice','https://tinyurl.com/duckSaladImage','Y', 'Laab Ped (Minced Duck Salad)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(14, 'ASIAN', 'Served with chicken thigh and leg meat and garnished with green onion, dried chile, Thai basil, lemongrass, and hon shimeji mushrooms.','https://tinyurl.com/AsianChickenSoupImage','Y', 'Gaeng Som (Chicken Soup With Fish Sauce and Tamarind)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(33, 'ASIAN', 'Chicken-broth soup with a red curry base that includes coconut milk','https://tinyurl.com/noodleSoupImage','Y', 'Khao Poon(Soup With Fermented Noodles)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(42, 'ASIAN', 'Charred in a wok with dark soy, ginger, garlic, onion, bell pepper, and mushrooms','https://tinyurl.com/SourPorkImage','Y',        'Muu Som (Rice-Fermented Sour Pork)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(6, 'ASIAN', 'Catfish fillets are mixed with rice flour and fried until crispy','https://tinyurl.com/crispyCatfishImage','Y', 'Paa Tod(Crispy catfish)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(12, 'ASIAN', 'Butter chicken is like pizza, hot dogs, and grilled cheese sandwiches: Even when it’s bad, it’s good.','https://tinyurl.com/butterChickenImage','Y', 'Butter Chicken', 10);
+
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(23, 'AFRICAN', 'The sauce in this dish gets its kick from berbere, an Ethiopian chile powder fragrant with cardamom, fenugreek, and clove',        'https://tinyurl.com/BefTibsImage','Y', 'Beef Tibs ', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(7, 'AFRICAN', 'Mandazi are coconut doughnuts that are traditionally served alongside bharazi, or pigeon peas stewed in coconut cream.',        'https://tinyurl.com/coconutDoughnutesImage','Y', 'Mandazi (East African Coconut Doughnuts)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(16, 'AFRICAN', 'Carrot soup with harissa and ginger.','https://tinyurl.com/spicySoupImage','Y', 'Spicy Carrot and Ginger Soup With Harissa',       10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(29, 'AFRICAN', 'Lamb meatball tagine, made with tender, big balls of ground lamb, flavored with carrots, shallots, tomatoes, golden raisins, cilantro, mint','https://tinyurl.com/LambMeatballmage','Y', 'Moroccan Lamb Meatball Tagine', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(41, 'AFRICAN', 'Well-spiced nuggets of beef or lamb are briefly fried in olive oil before being simmered in a simple, fragrant sauce of broth, onions, and tomato paste','https://tinyurl.com/tunisianMeatballsImage','Y', 'Tunisian Meatballs ', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(26, 'AFRICAN', 'Exciting contrast between sweet and spicy, harissa infused salmon with garlic and coriander',       'https://tinyurl.com/harissaSalmonImage','Y', 'Harissa-Honey Glazed Roasted Salmon', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(11, 'AFRICAN', 'Panko-dusted scallops, accented with lemon zest and cilantro. ','https://tinyurl.com/bakedScallopsImage','Y', 'Moroccan Baked Scallops', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(27, 'AFRICAN', 'Vegetarian shakshuka is a one-pan egg and tomato dish perfect for breakfast, lunch, or dinner.','https://tinyurl.com/poachedEggsImage','Y', 'Shakshuka (North African–Style Poached Eggs in Spicy Tomato Sauce) ', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(15, 'AFRICAN', 'The soup is accented by a sautéed onion, a bit of ground cumin, minced garlic, and a few squeezes of lemon juice. ',             'https://tinyurl.com/chickpeaSpupImage','Y', 'Lablabi (Tunisian Chickpea Soup)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(18, 'AFRICAN', 'Individual grains of rice that are cooked through but not mushy interspersed with tender, well-seasoned vegetables that hold their shape.','https://tinyurl.com/nigerianRiceImage','Y', 'Nigerian Fried Rice', 10);
+
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(53, 'ITALIAN', 'Its bright flavors come from garlic, basil, and cherry tomatoes, which we sauté until they burst.','https://tinyurl.com/cherryTomatoImage','Y', 'Pasta With Blistered Cherry Tomato Sauce', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(35, 'ITALIAN', 'Dressed up with browned butter, fresh sage, and tender chunks of butternut squash','https://tinyurl.com/SquashPastaImage',         'Y', 'Pasta With Butternut Squash and Sage Brown Butter', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(44, 'ITALIAN', 'Meat-free, egg-free, dairy-free, and still absolutely delicious.','https://tinyurl.com/carbonaraPastaImage','Y', 'Vegan Carbonara Pasta', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(8, 'ITALIAN', 'Arrabbiata is Italian for "angry," and embrace that fieriness by adding lots of red pepper flakes.','https://tinyurl.com/arrabbiataSauceImage','Y', 'Penne With Hot-As-You-Dare Arrabbiata Sauce', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(40, 'ITALIAN', 'Traditional Genovese preparation of pasta with pesto, green beans.','https://tinyurl.com/pestoPastaImage','Y', 'Pesto Pasta With Potatoes and Green Beans', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(45, 'ITALIAN', 'We incorporate just a little cream, turning instead to pasta water, cornstarch, and an egg to achieve a brighter, but still thick, sauce.','https://tinyurl.com/fettuccineAlfredoImage','Y', 'Lighter Fettuccine Alfredo', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(30, 'ITALIAN', 'This simple vegetarian dish of whole wheat fusilli uses both leaves and stems of Swiss chard.','https://tinyurl.com/citrusPastaImage','Y', 'Vegetarian Citrus Pasta With Swiss Chard', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(36, 'ITALIAN', 'Celebrate the coming of warmer days by pairing fresh asparagus with heavy cream, Parmigiano Reggiano, crispy prosciutto, and bright lemon. ','https://tinyurl.com/gnocchiProscuiuttoImage','Y', 'Ricotta Gnocchi With Asparagus and Prosciutto', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(10, 'ITALIAN', 'Browned mushrooms; white wine, shrimp and spinach. ',       'https://tinyurl.com/orecchietteShrimpImage','Y', 'One-Skillet Orecchiette With Shrimp, Spinach, and Mushrooms', 10);
+
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(39, 'CARIBEAN', 'Our version of ropa vieja (Cuban shredded stewed beef) improves efficiency and flavor.','https://tinyurl.com/ropaViejaImage','Y', 'Ropa Vieja Recipe', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(1, 'CARIBEAN', 'Three meats are pressed with cheese, pickles, and mustard until toasty and warm.','https://tinyurl.com/cubanSandwichImage','Y', 'Cuban Sandwiches', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(46, 'CARIBEAN', 'Classic Cuban sides for all kinds of meals.','https://tinyurl.com/blackBeansImage','Y', 'Cuban Black Beans and Rice Recipe',       10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(17, 'CARIBEAN', 'Tender stewed chicken in a rich and silky gravy.','https://tinyurl.com/stewChickenImage','Y', 'Jamaican Brown Stew Chicken', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(50, 'CARIBEAN', 'Marinated chicken, smoked low and slow on the grill','https://tinyurl.com/SpicyGrilledChickenImage','Y',       'Spicy Grilled Jerk Chicken', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(9, 'CARIBEAN', 'Spicy Jamaican chicken curry.','https://tinyurl.com/chickenCurryImage','Y', 'Jamaican Chicken Curry Recipe ', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(31, 'CARIBEAN', 'When you combine tacos and Jamaican beef patties, something magical happens.','https://tinyurl.com/jamaicanBeefTacosImage',       'Y', 'Jamaican Beef Tacos With Tropical Slaw', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(51, 'CARIBEAN', 'Jerk chicken has it all: sweetness, spice and come-hither appeal.','https://tinyurl.com/chickenRummyImage','Y', 'Hellfire Jerk Chicken With Rummy Grilled Pineapple', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(20, 'CARIBEAN', 'Curry-laced dough is filled with a heady, spicy beef mixture and baked in the oven until golden brown.','https://tinyurl.com/jamaicanPattiesImage','Y', 'Curried Jamaican Beef Patties', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(47, 'CARIBEAN', 'Onions, peppers, garlic, and fresh herbs are combined in the food processor for a powerfully flavorful Puerto Rican sofrito.','https://tinyurl.com/SofritoImage','Y', 'Puerto Rican Sofrito From Scratch', 10);
+
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(22, 'MEXICAN', 'Beef chuck and also oxtails, and then layers in even more flavor with multiple chilies and a splash of fish sauce.',              'https://tinyurl.com/beefiestBarbacoaImage','Y', 'Beefiest Barbacoa', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(48, 'MEXICAN', 'Traditional carnitas are made by slowly cooking pork in a pool of lard.','https://tinyurl.com/carnitasImage','Y', 'Killer Carnitas', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(38, 'MEXICAN', 'Mexican-style chile verde made with Hatch chilies','https://tinyurl.com/chileVerdeImage','Y', 'Chile Verde Virtuosity', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(52, 'MEXICAN', 'Made with pork shoulder and a handful of surprise characters, like raisins, orange juice from concentrate, and fish sauce.',
+       'https://tinyurl.com/carneAdobadaImage','Y', 'Carne Adovada', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(49, 'MEXICAN', 'A combination of poblano, cubanelle and jalapeno peppers.','https://tinyurl.com/chileVerde2Image','Y', 'Chile Verde With Pork', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(19, 'MEXICAN', 'The best way to make tacos for a crowd is to make tacos de canasta.','https://tinyurl.com/tacosCanastaImage','Y', 'Tacos de Canasta (Basket Tacos for a Party or Potluck)', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(55, 'MEXICAN', 'Chorizo Tacos combined with garlic, cumin, some warm spices, oregano, and vinega','https://tinyurl.com/chorizoTacosImage',       'Y', 'Mexican Chorizo Tacos', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(37, 'MEXICAN', 'Slow braised pork, shredded and then crisped up before serving.','https://tinyurl.com/shreddedPorkImage','Y', 'Shredded Pork in Ancho-Orange Sauce', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(21, 'MEXICAN', 'A blend of chiles and spices provide this classic Texas chili con carne with maximum depth of flavor.','https://tinyurl.com/chileCarneImage','Y', 'Original Texas Chili Con Carne', 10);
+INSERT INTO food (id, cuisine, description, image_url, is_enabled, food_name, price)
+values(4, 'MEXICAN', 'Grilled skirt steak, well salted, and deeply browned on the edges, with beefy brawn shining through underneath',       'https://tinyurl.com/carneAsadaTortaImage','Y', 'Carne Asada Torta', 10);
+
+
+insert into user(id,contact,email,password, profile, username, credit, name) values (1,"(214) 000-0001","hungrymike@gmail.com", "user123", "C",
+                                                                                     "mike", "", "Mike Rice");
+insert into user(id,contact,email,password, profile, username, credit, name) values (2,"(214) 000-0002","admin@foodbox.com", "admin123","A", "admin",
+                                                                                     "", "I am the Admin");
 
 
  insert into box (id, food_id, quantity)values(1,1,1);
